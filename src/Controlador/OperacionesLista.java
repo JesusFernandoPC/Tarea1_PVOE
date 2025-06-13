0f8427755ad9a09e39c7030e2858e1460236f754
rockl <rockl@Drag0nOnTheGo>
2025-06-13 02:21:13 -0600

    Agregue el metodo para buscar por licenciatura el cual muestra a todos los alumnos de una licenciatura

--- src/Controlador/OperacionesLista.java
+++ src/Controlador/OperacionesLista.java
@@ -15,6 +15,7 @@ public class OperacionesLista {
 
     // Clase interna Nodo
     private class Nodo {
+
         private Alumno alumno;
         private Nodo siguiente;
 
@@ -22,7 +23,7 @@ public class OperacionesLista {
             this.alumno = alumno;
             this.siguiente = null;
         }
-
+        
         public Alumno getAlumno() {
             return alumno;
         }
@@ -52,7 +53,6 @@ public class OperacionesLista {
         }
     }
 
-
     // Buscar alumno por matrícula
     public Alumno buscarPorMatricula(String matricula) {
 
@@ -61,38 +61,49 @@ public class OperacionesLista {
 
     // Buscar alumnos por licenciatura
     public void buscarPorLicenciatura(String claveLic) {
-
+        
+        Nodo lic =cabeza;//inicia el nodo desde la cabez
+        if(lic!=null){//compara el primer valor en la lista para saber si forma parte de la licenciatura buscada
+            if (claveLic.equals(lic.getAlumno().getLicenciatura())) {
+                System.out.println(lic.getAlumno().toString());
+            }
+        }
+        //avanza un nodo en la lista y comienza a comparar, siempre y cuando el siguiente nodo no este vacio
+        while(lic.getSiguiente()!=null){ 
+            lic=lic.getSiguiente();
+            if (claveLic.equals(lic.getAlumno().getLicenciatura())) {//compara la licenciatura buscada con la del alumno
+                System.out.println(lic.getAlumno().toString());//imprime los datos del alumno
+            }   
+        }
     }
 
     // Buscar alumnos por género
     public void buscarPorGenero(String genero) {
 
     }
-    
-    
+
     // Eliminar alumno por matrícula
-   public boolean eliminarPorMatricula(String matricula) {  
-       if (cabeza == null) {                //en caso de que se proporcione una lista sin nada
-           System.out.println("La lista está vacía.");
-           return false;
-       }
+    public boolean eliminarPorMatricula(String matricula) {
+        if (cabeza == null) {                //en caso de que se proporcione una lista sin nada
+            System.out.println("La lista está vacía.");
+            return false;
+        }
         // Caso mas facil : alumno al principio de la lista
         if (cabeza.getAlumno().getMatricula().equals(matricula)) {
             cabeza = cabeza.getSiguiente(); // Se elimina el primer nodo(cabeza)
             return true;                //(si se eliminó al alumno)
         }
-    // Recorremos la lista buscando al alumno
-    Nodo actual = cabeza;
-    while (actual.getSiguiente() != null) {         //mientras haya alumnos en la lista
-        if (actual.getSiguiente().getAlumno().getMatricula().equals(matricula)) {
-            actual.setSiguiente(actual.getSiguiente().getSiguiente()); // Se elimina el nodo
-            return true;            //(si se eliminó al alumno)
+        // Recorremos la lista buscando al alumno
+        Nodo actual = cabeza;
+        while (actual.getSiguiente() != null) {         //mientras haya alumnos en la lista
+            if (actual.getSiguiente().getAlumno().getMatricula().equals(matricula)) {
+                actual.setSiguiente(actual.getSiguiente().getSiguiente()); // Se elimina el nodo
+                return true;            //(si se eliminó al alumno)
+            }
+            actual = actual.getSiguiente();
         }
-        actual = actual.getSiguiente();
+        //el alumno no existe
+        return false;
     }
-    //el alumno no existe
-    return false;
-}
 
 }
-
